b46b42a3eb7bca2bb2a36dd5abeb8126aa14ab42
Timo Tijhof <krinklemail@gmail.com>
2012-04-30 19:13:03 +0200

    update
    - Fix typo, follows-up 0bd7bdc5bb0f75155586cd03591da476b2dad56e
    
    - Add error handling for addjob
    
    - Override action in addjobParams afterwards
    
    - Document default required parameter in testswarm.js
      There were some references to it in that function but it wasn't clear where they were
      defined. Added little comment.
    
    - Minor spacing, variable naming and EOF new line.
    
    - Turn browserSets into an array.
    
    - Add link to https://github.com/jquery/testswarm/tree/master/scripts/addjob#fields
      in the README.

--- test.js
+++ test.js
@@ -1,19 +1,17 @@
-var testBase = "http://localhost/testswarm-ui-checkout/";
+var myCode = "http://localhost/testswarm-ui-checkout/";
 require( "./testswarm" )( {
-	url: "http://localhost:82/testswarm/",
+	url: "http://localhost/testswarm/",
 	pollInterval: 1000,
-	timeout: 1000 * 10,
+	timeout: 1000 * 60 * 10,
 	done: function( passed ) {
 		process.exit( passed ? 0 : 1 );
 	}
 }, {
 	authUsername: "jqueryui",
-	authToken: "71d46254c5c4dd883592ee3188593a340aaecb0f",
-	jobName: 'jQuery UI commit',
+	authToken: "89e495e7941cf9e40e6980d14a16bf023ccd4c91",
+	jobName: "jQuery UI commit #c3499c27",
 	runMax: 1,
 	"runNames[]": ["Accordion", "Autocomplete"],
-	"runUrls[]": [ testBase + "tests/unit/accordion/accordion.html", testBase + "tests/unit/autocomplete/autocomplete.html" ],
-	// "runNames[]": ["Autocomplete"],
-	// "runUrls[]": [ testBase + "tests/unit/autocomplete/autocomplete.html" ],
-	browserSets: "popular"
+	"runUrls[]": [ myCode + "/tests/unit/accordion/accordion.html", myCode + "/tests/unit/autocomplete/autocomplete.html" ],
+	"browserSets[]": ["popular"]
 });
